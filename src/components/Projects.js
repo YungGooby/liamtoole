371db9f327315e7bdecdf5d5ee723c06ed2a1873
YungGooby <liam_toole@hotmail.co.uk>
2021-09-01 12:39:06 +0100

    styling on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,37 @@
 // import ProjectsBlock from "./ProjectsBlock";
 import ProjectCard from "./ProjectCard";
-import testImage from './images/testImage.PNG'
+import logo from "../logo.svg";
+import svelteImage from "./images/rsz_svelteimage.png";
+import discImage from "./images/rsz_discordimage.jpeg"
 
 function Projects() {
     return (
         <>
             <h1>Projects being worked on</h1>
-            <hr className="solid"/>
-            <ProjectCard
-            link="/discordscrape"
-            image="https://parentzone.org.uk/sites/default/files/discord_logo_wordmark_2400.0.jpg"
-            title="Discord Chat Scraper"
-            cat="Python"/>
-            <ProjectCard
-            link="/"
-            image={testImage}
-            title="Portfolio"
-            cat="Website"/>
+            <hr className="solid" />
+            <div className="proj-container">
+                <ProjectCard
+                    link="/discordscrape"
+                    image={discImage}
+                    title="Discord Chat Scraper"
+                    cat="Python, Discord"
+                    complete="false"
+                />
+                <ProjectCard
+                    link="/"
+                    image={logo}
+                    title="Portfolio"
+                    cat="Website, React, HTML, CSS"
+                    complete="false"
+                />
+                <ProjectCard
+                    link="/"
+                    image={svelteImage}
+                    title="Svelte Practice"
+                    cat="Svelte"
+                    complete="false"
+                />
+            </div>
         </>
     );
 }
