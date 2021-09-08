c8b241250627bba3acae30032c3604d51d88e761
YungGooby <liam_toole@hotmail.co.uk>
2021-09-08 16:27:30 +0100

    added content to the mobile homepage

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -34,10 +34,16 @@ const Terminal = () => {
     );
 };
 
-const MobileContent = () =>{
-    return(
+const MobileContent = () => {
+    return (
         <>
+            <div className="homecontent-mobile">
+                <h1>Fun</h1>
+                <h1>Frustrating</h1>
+                <h1>Challenging</h1>
+                <h1>Rewarding</h1>
+            </div>
         </>
-    )
-}
+    );
+};
 export default HomeContent;
