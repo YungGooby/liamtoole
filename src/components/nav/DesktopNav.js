cae191cefd5682c14e6b5ebac28d1d2d1c3751bd
YungGooby <liam_toole@hotmail.co.uk>
2021-09-22 14:07:09 +0100

    added motion elements to navigation bar for desktop

--- src/components/nav/DesktopNav.js
+++ src/components/nav/DesktopNav.js
@@ -1,37 +1,48 @@
 import { Link } from "react-router-dom";
 import Toggle from "../toggle/Toggle";
-import logo from "../images/logo.svg"
+import logo from "../images/logo.svg";
+import { motion } from "framer-motion";
 
 const DesktopNav = () => {
     return (
-        <div className="navigation">
-            <Link to="/">
-                <img src={logo} className="logo" alt="logo" />
+        <div className='navigation'>
+            <Link to='/'>
+                <img src={logo} className='logo' alt='logo' />
             </Link>
-            <div className="navigation-sub">
-                <Link to="/about" className="item">
-                    [ about ]
-                </Link>
-                <Link to="/projects" className="item">
-                    [ projects ]
-                </Link>
-                <a
-                    href="https://github.com/YungGooby"
-                    className="item"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    [ github ]
-                </a>
-                <a
-                    href="https://github.com/YungGooby"
-                    className="item"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    [ cv ]
-                </a>
-                <Toggle/>
+            <div className='navigation-sub'>
+                <motion.a whileTap={{ scale: 0.9 }}>
+                    <Link to='/about' className='item'>
+                        [ about ]
+                    </Link>
+                </motion.a>
+                <motion.a whileTap={{ scale: 0.9 }}>
+                    <Link to='/projects' className='item'>
+                        [ projects ]
+                    </Link>
+                </motion.a>
+                <motion.a whileTap={{ scale: 0.9 }}>
+                    <a
+                        href='https://github.com/YungGooby'
+                        className='item'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        [ github ]
+                    </a>
+                </motion.a>
+                <motion.a whileTap={{ scale: 0.9 }}>
+                    <a
+                        href='https://github.com/YungGooby'
+                        className='item'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        [ cv ]
+                    </a>
+                </motion.a>
+                <motion.a whileTap={{ scale: 0.9 }}>
+                    <Toggle />
+                </motion.a>
             </div>
         </div>
     );
