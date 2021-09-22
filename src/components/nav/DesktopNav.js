d9519f7993f16006c43efae4803106ea11b2d74a
YungGooby <liam_toole@hotmail.co.uk>
2021-09-22 16:54:40 +0100

    corrected some spelling errors on the about us page

--- src/components/nav/DesktopNav.js
+++ src/components/nav/DesktopNav.js
@@ -10,17 +10,17 @@ const DesktopNav = () => {
                 <img src={logo} className='logo' alt='logo' />
             </Link>
             <div className='navigation-sub'>
-                <motion.a whileTap={{ scale: 0.9 }}>
+                <motion.span whileTap={{ scale: 0.9 }}>
                     <Link to='/about' className='item'>
                         [ about ]
                     </Link>
-                </motion.a>
-                <motion.a whileTap={{ scale: 0.9 }}>
+                </motion.span>
+                <motion.span whileTap={{ scale: 0.9 }}>
                     <Link to='/projects' className='item'>
                         [ projects ]
                     </Link>
-                </motion.a>
-                <motion.a whileTap={{ scale: 0.9 }}>
+                </motion.span>
+                <motion.span whileTap={{ scale: 0.9 }}>
                     <a
                         href='https://github.com/YungGooby'
                         className='item'
@@ -29,8 +29,8 @@ const DesktopNav = () => {
                     >
                         [ github ]
                     </a>
-                </motion.a>
-                <motion.a whileTap={{ scale: 0.9 }}>
+                </motion.span>
+                <motion.span whileTap={{ scale: 0.9 }}>
                     <a
                         href='https://github.com/YungGooby'
                         className='item'
@@ -39,10 +39,10 @@ const DesktopNav = () => {
                     >
                         [ cv ]
                     </a>
-                </motion.a>
-                <motion.a whileTap={{ scale: 0.9 }}>
+                </motion.span>
+                <motion.span whileTap={{ scale: 0.9 }}>
                     <Toggle />
-                </motion.a>
+                </motion.span>
             </div>
         </div>
     );
